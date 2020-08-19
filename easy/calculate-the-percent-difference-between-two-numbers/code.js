818bde7a1110f3ac6c2e6bee9f9ef4c4acb4dda1
James Ouma <luc.mcdowellin@gmail.com>
2020-08-18 23:54:23 -0500

    calculate percent difference between 2 numbers

--- easy/calculate-the-percent-difference-between-two-numbers/code.js
+++ easy/calculate-the-percent-difference-between-two-numbers/code.js
@@ -1,5 +1,9 @@
 function percentDiff(num1, num2) {
   // Your code here.
+  let diff1 = num1 - num2;
+  let halfSum = (num1 + num2)/2;
+  return Math.round(100 * Math.abs(diff1 / (halfSum) *10 ))/10;
 }
-
+// console.log(percentDiff(60, 100))
+// console.log(percentDiff(5439, 4538))
 module.exports = percentDiff;
